60e7bb466ed2f291ff1966676877ffe21329af6f
rsacramento <ricardo_sacramento@outlook.com>
2022-07-18 18:53:39 +0100

    adding stuf

---
+++ nvim/.config/nvim/lua/sacramento/indent-blankline.lua
@@ -0,0 +1,9 @@
+if not pcall(require, 'indent_blankline') then
+  return
+end
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
